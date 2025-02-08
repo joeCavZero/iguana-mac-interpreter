20278626ff2ed9746042cf81e71c7ab596a8dea7
joeCavZero <joaocavalcantejcavalcante@gmail.com>
2025-02-08 02:44:23 -0300

    [feat] now the logkit identify if your terminal supports colors

--- src/interpreter/logkit.rs
+++ src/interpreter/logkit.rs
@@ -1,25 +1,53 @@
 use colored::Colorize;
+use supports_color::Stream;
 
 const INTERPRETER_NAME : &str = "Interpreter";
 
 pub fn exit_with_positional_error_message(message: &str, line: u32, col: u32) {
-    println!(
-        "{} {} {} {}",
-        
-        format!("[{}]", INTERPRETER_NAME).bold().yellow(),
-        "[ERROR]".bold().red(),
-        message,
-        format!("[LINE: {}, COL: {}]", line, col).bold().cyan(),
-    );
+    if let Some(color_level) = supports_color::on(Stream::Stdout) {
+        if color_level.has_16m {
+            println!(
+                "{} {} {} {}",
+                format!("[{}]", INTERPRETER_NAME).bold().yellow(),
+                "[ERROR]".bold().red(),
+                message,
+                format!("[LINE: {}, COL: {}]", line, col).bold().cyan(),
+            );
+        } else {
+            println!(
+                "[{}] [ERROR] {} [LINE: {}, COL: {}]",
+                INTERPRETER_NAME, message, line, col
+            );
+        }
+    } else {
+        println!(
+            "[{}] [ERROR] {} [LINE: {}, COL: {}]",
+            INTERPRETER_NAME, message, line, col
+        );
+    }
     std::process::exit(0);
 }
 
 pub fn exit_with_error_message(message: &str) {
-    println!(
-        "{} {} {}",
-        format!("[{}]", INTERPRETER_NAME).bold().yellow(),
-        "[ERROR]".bold().red(),
-        message,
-    );
-    std::process::exit(0);
+
+    if let Some(color_level) = supports_color::on(Stream::Stdout) {
+        if color_level.has_16m {
+            println!(
+                "{} {} {}",
+                format!("[{}]", INTERPRETER_NAME).bold().yellow(),
+                "[ERROR]".bold().red(),
+                message,
+            );
+        } else {
+            println!(
+                "[{}] [ERROR] {}",
+                INTERPRETER_NAME, message
+            );
+        }
+    } else {
+        println!(
+            "[{}] [ERROR] {}",
+            INTERPRETER_NAME, message
+        );
+    }
 }

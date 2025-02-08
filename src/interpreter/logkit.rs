aa87c1913ff6d4ff28629edaeaef2a88b9d1ff64
joeCavZero <joaocavalcantejcavalcante@gmail.com>
2025-02-08 14:11:41 -0300

    [refac] logkit got polished

--- src/interpreter/logkit.rs
+++ src/interpreter/logkit.rs
@@ -3,51 +3,65 @@ use supports_color::Stream;
 
 const INTERPRETER_NAME : &str = "Interpreter";
 
-pub fn exit_with_positional_error_message(message: &str, line: u32, col: u32) {
+
+fn interpreter_name_piece() -> String {
+    let name_piece = format!("[{}]", INTERPRETER_NAME);
     if let Some(color_level) = supports_color::on(Stream::Stdout) {
         if color_level.has_16m {
-            println!(
-                "{} {} {} {}",
-                format!("[{}]", INTERPRETER_NAME).bold().yellow(),
-                "[ERROR]".bold().red(),
-                message,
-                format!("[LINE: {}, COL: {}]", line, col).bold().cyan(),
-            );
+            name_piece.bold().yellow().to_string()
         } else {
-            println!(
-                "[{}] [ERROR] {} [LINE: {}, COL: {}]",
-                INTERPRETER_NAME, message, line, col
-            );
+            name_piece
         }
     } else {
-        println!(
-            "[{}] [ERROR] {} [LINE: {}, COL: {}]",
-            INTERPRETER_NAME, message, line, col
-        );
+        name_piece
     }
+}
+
+fn error_piece() -> String {
+    let error_piece = "[ERROR]";
+    if let Some(color_level) = supports_color::on(Stream::Stdout) {
+        if color_level.has_16m {
+            error_piece.bold().red().to_string()
+        } else {
+            error_piece.to_string()
+        }
+    } else {
+        error_piece.to_string()
+    }
+}
+
+fn line_col_piece(line: u32, col: u32) -> String {
+    let line_col_piece = format!("[LINE: {}, COL: {}]", line, col);
+    if let Some(color_level) = supports_color::on(Stream::Stdout) {
+        if color_level.has_16m {
+            line_col_piece.bold().cyan().to_string()
+        } else {
+            line_col_piece
+        }
+    } else {
+        line_col_piece
+    }
+}
+
+pub fn exit_with_positional_error_message(message: &str, line: u32, col: u32) {  
+    println!(
+        "{} {} {} {}",
+        interpreter_name_piece(), 
+        error_piece(),
+        message, 
+        line_col_piece(line, col),
+    );
+
     std::process::exit(0);
 }
 
-pub fn exit_with_error_message(message: &str) {
 
-    if let Some(color_level) = supports_color::on(Stream::Stdout) {
-        if color_level.has_16m {
-            println!(
-                "{} {} {}",
-                format!("[{}]", INTERPRETER_NAME).bold().yellow(),
-                "[ERROR]".bold().red(),
-                message,
-            );
-        } else {
-            println!(
-                "[{}] [ERROR] {}",
-                INTERPRETER_NAME, message
-            );
-        }
-    } else {
-        println!(
-            "[{}] [ERROR] {}",
-            INTERPRETER_NAME, message
-        );
-    }
+pub fn exit_with_error_message(message: &str) {
+    println!(
+        "{} {} {}",
+        interpreter_name_piece(),
+        error_piece(),
+        message
+    ); 
+    std::process::exit(0);
 }

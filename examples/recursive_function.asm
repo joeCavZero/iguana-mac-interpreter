97b621d33bcfccdc0e79b56816b2e8b251ca4bba
joeCavZero <joaocavalcantejcavalcante@gmail.com>
2025-03-27 22:42:30 -0300

    [feat] recursive function example added

---
+++ examples/recursive_function.asm
@@ -0,0 +1,37 @@
+# THIS PROGRAM IMPLEMENTS A RECURSIVE FUNCTION
+# THAT USE THE STACK TO STORE THE FUNCTION ARG
+# AND THE RETURNED VALUE
+.data
+    ONE: .word 0b01
+    MAX: .word 200
+    INCR: .word 5
+    NEW_LINE: .ascii "\n"
+.text
+    INSP 1 # increases a space to store the function argument
+    LOCO 0 # this number is the function argument
+    STOL 0 # store the function argument
+    CALL RECURSIVE_FUNCTION
+    LODD NEW_LINE PRINTACCHAR
+    LODL -2
+    PRINTLNAC
+    HALT
+RECURSIVE_FUNCTION:
+    INSP 1
+    # [ARG, RTN, ~]
+    LODL 2
+    STOL 0
+    ADDD INCR
+    STOL 0
+    # [ARG, RTN, ARG+INCR]
+    LODL 0
+    PRINTLNAC SLEEPI 100
+    SUBD MAX
+    JPOS RECURSIVE_FUNCTION_REGRESS
+    CALL RECURSIVE_FUNCTION
+  RECURSIVE_FUNCTION_REGRESS:
+    # [ARG, RTN, RESULT]
+    LODL 0
+    STOL 2
+    DESP 1
+    # [RESULT, RTN]
+    RETN
